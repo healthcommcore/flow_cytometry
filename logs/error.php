83ab3e8b8ce18271b113bf103d3fd64a269b4b0e
hccadmin <dave_rothfarb@dfci.harvard.edu>
2012-11-27 16:44:03 -0500

    initial commit

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2012-08-20 21:48:54 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2012-08-20	21:48:54	INFO	155.52.12.170	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
